1676a880a8f110abd6c1affbf20d59bfcb3f6f45
Denis Le <dl.denisle@gmail.com>
2025-02-22 17:40:53 -0500

    Added button component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "./components/home/home";
 import NavBar from "./components/navbar";
+import LandingPage from "./components/landing-page/landing-page";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <NavBar/>
     <Routes>
       <Route path="/" element={<Home/>}/>
+      <Route path="/landing" element={<LandingPage/>}/>
     </Routes>
     </BrowserRouter>
     </>
---
+++ frontend/src/components/shared-components/button.jsx
@@ -0,0 +1,10 @@
+export default function Button({ text, onClick, className }) {
+  return (
+    <button
+      className="bg-blue-700 hover:bg-blue-800 text-white py-2 px-4 rounded-lg"
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+}
