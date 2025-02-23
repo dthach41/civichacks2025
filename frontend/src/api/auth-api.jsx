3d9cf9c63dd5de68dc31d48ccc1088b7dd66f10b
dthach41 <thachderek1128@gmail.com>
2025-02-23 10:22:33 -0500

    fix env vars

--- frontend/src/api/auth-api.jsx
+++ frontend/src/api/auth-api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api';
+const API_URL = import.meta.env.API + "/api";
 
 export const register = async (name, email, password, jobSeeking) => {
     const response = await axios.post(API_URL + '/auth/register', {
