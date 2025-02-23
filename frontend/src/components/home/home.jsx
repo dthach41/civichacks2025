939f52bb6248fde4b3610b271e3bba7db768ccc8
Denis Le <dl.denisle@gmail.com>
2025-02-23 00:21:01 -0500

    Add second carousel

--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -15,43 +15,73 @@ const responsive = {
     superLargeDesktop: {
       breakpoint: { max: 4000, min: 1024 },
       items: 3,
-      partialVisibilityGutter: 20
+      partialVisibilityGutter: 40
     },
     desktop: {
       breakpoint: { max: 1024, min: 768 },
       items: 2,
-      partialVisibilityGutter: 15
+      partialVisibilityGutter: 30
     },
     tablet: {
       breakpoint: { max: 768, min: 464 },
       items: 1,
-      partialVisibilityGutter: 10
+      partialVisibilityGutter: 20
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
       items: 1,
-      partialVisibilityGutter: 5
+      partialVisibilityGutter: 10
     },
   };
 
-const JobCarousel = ({ title }) => {
+const JobCarousel = () => {
     return (
-      <div className="my-6 max-w-6xl mx-auto">
-        <h2 className="text-lg font-semibold text-center mb-4">{title}</h2>
-        <Carousel className="py-4"
-          responsive={responsive}
-          infinite={true}
-          autoPlay={false}
-          keyBoardControl={true}
-          showDots={false}
-          removeArrowOnDeviceType={["tablet", "mobile"]}
-          itemClass="px-2 flex justify-center w-[90%]"
-        >
-          {jobs.map((job) => (
-            <JobCard key={job.id} title={job.title} description="Job Description" />
-          ))}
-        </Carousel>
-      </div>
+      <>
+        <div className="my-6 max-w-6xl mx-auto">
+          <h1 className="text-3xl font-bold text-gray-800 mb-6 px-4">Jobs</h1>
+          <h2 className="text-lg font-semibold text-center mb-4">Recommended Jobs</h2>
+          <Carousel
+            className="py-4"
+            responsive={responsive}
+            infinite={true}
+            autoPlay={false}
+            keyBoardControl={true}
+            showDots={false}
+            removeArrowOnDeviceType={["tablet", "mobile"]}
+            itemClass="px-2 flex justify-center w-[85%]"
+          >
+            {jobs.map((job) => (
+              <JobCard
+                key={job.id}
+                title={job.title}
+                description="Job Description"
+              />
+            ))}
+          </Carousel>
+          <hr className="h-0.5 my-8 bg-gray-200 border-0 dark:bg-gray-500"></hr>
+        </div>
+        <div className="my-6 max-w-6xl mx-auto">
+          <h2 className="text-lg font-semibold text-center mb-4">Your Jobs</h2>
+          <Carousel
+            className="py-4"
+            responsive={responsive}
+            infinite={true}
+            autoPlay={false}
+            keyBoardControl={true}
+            showDots={false}
+            removeArrowOnDeviceType={["tablet", "mobile"]}
+            itemClass="px-2 flex justify-center w-[85%]"
+          >
+            {jobs.map((job) => (
+              <JobCard
+                key={job.id}
+                title={job.title}
+                description="Job Description"
+              />
+            ))}
+          </Carousel>
+        </div>
+      </>
     );
   };
   
